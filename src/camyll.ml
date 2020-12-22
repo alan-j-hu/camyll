fb09eb0980426de6ee5313b31c5e602f768c96d4
Alan Hu <alanh@ccs.neu.edu>
2020-12-21 22:49:21 -0500

    Add the new command for initializing a site

--- src/camyll.ml
+++ src/camyll.ml
@@ -1,5 +1,14 @@
 open Cmdliner
 
+let new_cmd =
+  let doc = "initialize a site" in
+  let arg =
+    let inf = Arg.info ~docv:"PATH" [] in
+    Arg.(required & pos 0 (some string) None inf)
+  in
+  Term.(ret (const New.new_ $ arg)),
+  Term.info "new" ~doc
+
 let build_cmd =
   let doc = "build the site" in
   Term.(ret (const Build.build $ const ())),
@@ -10,6 +19,6 @@ let default_cmd =
   Term.(ret (const (`Help(`Auto, None)))),
   Term.info "camyll" ~doc
 
-let cmds = [build_cmd]
+let cmds = [new_cmd; build_cmd]
 
 let main () = Term.(exit @@ eval_choice default_cmd cmds)
