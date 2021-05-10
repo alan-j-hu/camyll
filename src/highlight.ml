42b2d188b8336bba7dff16d93254d5f8c841bb94
Alan Hu <alanh@ccs.neu.edu>
2021-05-10 00:38:04 -0400

    Map TextMate token names to Pygments classes

--- src/highlight.ml
+++ src/highlight.ml
@@ -1,53 +1,95 @@
-let contains_at idx str prefix =
-  let pre_len = String.length prefix in
-  let str_len = String.length str in
-  if idx + pre_len > str_len then
-    false
-  else
-    String.sub str idx pre_len = prefix
-
-let begins_with = contains_at 0
-
-type rules_tree = Rule of string * string option * rules_tree list
-
-let rec eval_rules idx str = function
-  | [] -> None
-  | (Rule(name, klass, children)) :: rules ->
-    if contains_at idx str name then
-      match eval_rules (idx + String.length name) str children with
-      | Some a -> Some a
-      | None -> klass
-    else
-      eval_rules idx str rules
-
-let get_class str =
-  let rules =
-    [ Rule("comment", Some "co", [])
-    ; Rule("constant", None,
-           [ Rule(".numeric", Some "bn", [])
-           ; Rule(".language", Some "cn", []) ])
-    ; Rule("keyword", Some "kw",
-           [ Rule(".control", Some "cf", [])
-           ; Rule(".operator", Some "op", []) ])
-    ; Rule("entity", None,
-           [ Rule(".name", Some "va",
-                  [ Rule(".function", Some "fu", [])
-                  ; Rule(".type", Some "dt", []) ])
-           ]
-          )
-    ; Rule("variable", Some "va", [])
-    ]
-  in
-  eval_rules 0 str rules
-
 type exists_node = Node : 'a Soup.node -> exists_node
+
+(* This is the same mapping that is used in Pygments. See the code in
+   https://github.com/pygments/pygments/blob/6ded9db39463372e5205a36bea72d6de516ece69/pygments/token.py#L124
+   and
+   https://github.com/alecthomas/chroma/blob/9dc3c8c529257cd96c927ca7d2f35d83264c22e8/types.go#L228. *)
+let get_class = function
+  | "whitespace" :: _ -> Some "w"
+  | "escape" :: _ -> Some "esc"
+  | "error" :: _ -> Some "err"
+  | "other" :: _ -> Some "x"
+  | "keyword" :: "constant" :: _ -> Some "kc"
+  | "keyword" :: "declaration" :: _ -> Some "kd"
+  | "keyword" :: "namespace" :: _ -> Some "kn"
+  | "keyword" :: "pseudo" :: _ -> Some "kp"
+  | "keyword" :: "reserved" :: _ -> Some "kr"
+  | "keyword" :: "type" :: _ -> Some "kt"
+  | "keyword" :: _ -> Some "k"
+  | "name" :: "attribute" :: _ -> Some "na"
+  | "name" :: "builtin" :: "pseudo" :: _ -> Some "bp"
+  | "name" :: "builtin" :: _ -> Some "nb"
+  | "name" :: "class" :: _ -> Some "nc"
+  | "name" :: "constant" :: _ -> Some "no"
+  | "name" :: "decorator" :: _ ->  Some "nd"
+  | "name" :: "entity" :: _ -> Some "ni"
+  | "name" :: "exception" :: _ -> Some "ne"
+  | "name" :: "function" :: "magic" :: _ -> Some "fm"
+  | "name" :: "function" :: _ -> Some "nf"
+  | "name" :: "property" :: _ -> Some "py"
+  | "name" :: "label" :: _ -> Some "nl"
+  | "name" :: "namespace" :: _ -> Some "nn"
+  | "name" :: "other" :: _ -> Some "nx"
+  | "name" :: "tag" :: _ -> Some "nt"
+  | "name" :: "variable" :: "class" :: _ ->  Some "vc"
+  | "name" :: "variable.global" :: _ ->  Some "vg"
+  | "name" :: "variable.instance" :: _ -> Some "vi"
+  | "name" :: "variable" :: "magic" :: _ -> Some "vm"
+  | "name" :: "variable" :: _ ->  Some "nv"
+  | "name" :: _ -> Some "n"
+  | "literal" :: "date" :: _ ->  Some "ld"
+  | "string" :: "affix" :: _ -> Some "sa"
+  | "literal" :: _ -> Some "l"
+  | "string" :: "backtick" :: _ -> Some "sb"
+  | "string" :: "char" :: _ -> Some "sc"
+  | "string" :: "delimiter" :: _ -> Some "dl"
+  | "string" :: "doc" :: _ -> Some "sd"
+  | "string" :: "double" :: _ -> Some "s2"
+  | "string" :: "escape" :: _ -> Some "se"
+  | "string" :: "heredoc" :: _ -> Some "sh"
+  | "string" :: "interpol":: _ -> Some "si"
+  | "string" :: "other" :: _ ->  Some "sx"
+  | "string" :: "regex" :: _ ->  Some "sr"
+  | "string" :: "single" :: _ -> Some "s1"
+  | "string" :: "symbol" :: _ -> Some "ss"
+  | "string" :: _ -> Some "s"
+  | "number" :: "bin" :: _ -> Some "mb"
+  | "number" :: "float" :: _ -> Some "mf"
+  | "number" :: "hex" :: _ -> Some "mh"
+  | "number" :: "integer" :: "long" :: _ -> Some "il"
+  | "number" :: "integer" :: _ -> Some "mi"
+  | "number" :: "oct" :: _ -> Some "mo"
+  | "number" :: _ -> Some "m"
+  | "operator" :: "word" :: _ -> Some "ow"
+  | "operator" :: _ -> Some "o"
+  | "punctuation" :: _ -> Some "p"
+  | "comment" :: "hashbang" :: _ -> Some "ch"
+  | "comment" :: "multiline":: _ -> Some "cm"
+  | "comment" :: "preproc":: _ -> Some "cp"
+  | "comment" :: "preprocfile":: _ -> Some "cpf"
+  | "comment" :: "single" :: _ -> Some "c1"
+  | "comment" :: _ -> Some "c"
+  | "gomment" :: "special" :: _ -> Some "cs"
+  | "generic" :: "deleted" :: _ -> Some "gd"
+  | "generic" :: "emph" :: _ -> Some "ge"
+  | "generic" :: "error" :: _ -> Some "gr"
+  | "generic" :: "heading" :: _ -> Some "gh"
+  |" generic" :: "inserted" :: _ -> Some "gi"
+  | "generic" :: "output" :: _ -> Some "go"
+  | "generic" :: "prompt" :: _ -> Some "gp"
+  | "generic" :: "strong" :: _ -> Some "gs"
+  | "generic" :: "subheading" :: _ -> Some "gu"
+  | "generic" :: "traceback" :: _ -> Some "gt"
+  | "generic" :: _ -> Some "g"
+  | _ -> None
 
 let create_node scopes i j line =
   assert (j > i);
   let inner_text = String.sub line i (j - i) in
   let class_ = match scopes with
     | [] -> None
-    | scope :: _ -> get_class scope
+    | scope :: _ ->
+      get_class (String.split_on_char '.' (String.lowercase_ascii scope))
   in
   match class_ with
   | Some class_ -> Node (Soup.create_element ~class_ "span" ~inner_text)
@@ -60,6 +102,12 @@ let rec highlight_tokens i acc line = function
     let span = create_node (TmLanguage.scopes tok) i j line in
     highlight_tokens j (span :: acc) line toks
 
+let highlight_line langs grammar stack line =
+  let tokens, stack = TmLanguage.tokenize_exn langs grammar stack line in
+  let nodes = highlight_tokens 0 [] line tokens in
+  let a = Soup.create_element "a" ~class_:"sourceLine" in
+  List.iter (fun (Node node) -> Soup.append_child a node) nodes;
+  a, stack
 
 (** Maps over the list while keeping track of some state.
     Discards the state because I don't need it. *)
@@ -73,15 +121,8 @@ let highlight_block langs grammar code =
   let lines = String.split_on_char '\n' code in
   (* Some patterns don't work if there isn't a newline *)
   let lines = List.map (fun s -> s ^ "\n") lines in
-  let a's =
-    map_fold (fun stack line ->
-        let tokens, stack = TmLanguage.tokenize_exn langs grammar stack line in
-        let nodes = highlight_tokens 0 [] line tokens in
-        let a = Soup.create_element "a" ~class_:"sourceLine" in
-        List.iter (fun (Node node) -> Soup.append_child a node) nodes;
-        a, stack) TmLanguage.empty lines
-  in
-  let code = Soup.create_element "code" in
+  let a's = map_fold (highlight_line langs grammar) TmLanguage.empty lines in
+  let code = Soup.create_element "code" ~class_:"highlight" in
   List.iter (Soup.append_child code) a's;
   let pre = Soup.create_element "pre" in
   Soup.append_child pre code;
